93f58ae2a7711b87a020038a0d0825d26e789ce0
dpartner <d.partner.ua@gmail.com>
2026-03-06 19:43:00 +0200

    phone mask

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,3 @@
-export const PHONE_MASK = '+{54} (000) 000 - 0000';
-export const PHONE_REGEX = /^\+54\s?\(\d{3}\)\s\d{3}\s-\s\d{4}$/;
+export const PHONE_MASK = '+{54}(000) 000-0000';
+export const PHONE_REGEX = /^\+54\(\d{3}\)\s\d{3}-\d{4}$/;
 export const PHONE_PREFIX = '+54';
